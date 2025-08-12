9993ad4ec5e0fdf142f6123692ed09e80b531ce6
SuMayaBee <sumaiyaislam-14-2021711385@it.du.ac.bd>
2025-08-12 13:04:28 +0600

    7 days set

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Settings:
     SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecret")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 1 day
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
 
 settings = Settings()
